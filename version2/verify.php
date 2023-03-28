90966bdcd95ec62887c6941b9631184879de1406
Amigo51 <chopstiz@gmail.com>
2023-03-28 19:28:13 +0000

    made some corrections to the php file

--- version2/verify.php
+++ version2/verify.php
@@ -3,7 +3,7 @@
 // Hardcoded username and password
 $username = "anyone";
 $password = "password123";
-$email = "anyone@mail.com"
+$email = "anyone@mail.com";
 
 // Check if the form has been submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -11,10 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Get the submitted username and password
   $submittedUsername = $_POST['username'];
   $submittedPassword = $_POST['password'];
-  $submittedmail = $_POST['email';]
+  $submittedmail = $_POST['email'];
 
   // Verify the username and password
-  if ($submittedUsername == $username && $submittedPassword == $password &&$submittedmail=$email) {
+  if ($submittedUsername == $username && $submittedPassword == $password && $submittedmail== $email) {
     // Successful login
     echo "Welcome, " . $username . "!";
   } else {
