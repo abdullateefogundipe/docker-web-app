9e81a76442f76a424ba9e53146aab4de9668115c
Amigo51 <chopstiz@gmail.com>
2023-04-20 09:49:09 +0000

    Update anything.tf

--- version3/anything.tf
+++ version3/anything.tf
@@ -1,5 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+#Provider profile and region in which all the resources will create
 provider "aws" {
-    access_key = "Your Access Key"
-    secret_key = "Your Secret Key"
-    region = "region"
+  profile = "default"
+  region  = "us-east-1"
+}
+
+#Resource to create dynamodb table 
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "Employee"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "EmployeeId"
+
+  attribute {
+    name = "EmployeeId"
+    type = "S"
+  }
+
+  
+
+  tags = {
+    Name        = "Dynamo-DB-Table"
+    Environment = "Dev"
+  }
 }
